e8e033737f4789e5d0e7c5ac73f073d5cdba74f3
Abhishek <abhishelke297127@gmail.com>
2024-04-29 19:12:58 +0530

    all files setteled in various folder

---
+++ javascript/ass_05.js
@@ -0,0 +1,15 @@
+<!-- Scripts -->
+    function toggleSection(sectionId) {
+        var section = document.getElementById(sectionId);
+        var otherSections = document.querySelectorAll('.container section[id]:not(#' + sectionId + ')');
+
+        if (section.style.display === "none") {
+            section.style.display = "block";
+            // Hide other sections
+            otherSections.forEach(function(otherSection) {
+                otherSection.style.display = "none";
+            });
+        } else {
+            section.style.display = "none";
+        }
+    }
